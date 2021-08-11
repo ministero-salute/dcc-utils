19219ef5df132862df83509c0b4993b61fab7206
Andrea Stagi <stagi.andrea@gmail.com>
2021-08-11 11:13:56 +0200

    chore: add warning messages to examples

--- examples/verify_signature_from_list.js
+++ examples/verify_signature_from_list.js
@@ -1,5 +1,5 @@
 /*
-* Example data taken from covid-pass-verifier by Csongor Bokay 
+* Trust list taken from covid-pass-verifier by Csongor Bokay 
 * https://github.com/bcsongor/covid-pass-verifier
 */
 
@@ -10,7 +10,7 @@ const main = async function () {
   const TRUST_LIST_URL = 'https://raw.githubusercontent.com/bcsongor/covid-pass-verifier/35336fd3c0ff969b5b4784d7763c64ead6305615/src/data/certificates.json';
   const response = await fetch(TRUST_LIST_URL);
   const signatures = await response.json();
-  const dcc = await DCC.fromImage('./data/2.png');
+  const dcc = await DCC.fromImage('./data/2.png'); // ⚠️ Substitute with a valid Green Pass
   for (signature of signatures) {
     if (signature.pub) {
       try {
