afb65f29c96eb2ea3386eaf0e79d8b520c5f23c8
lucadentella <luca@dentella.it>
2021-11-02 21:57:17 +0100

    Get KID from cose headers

--- src/dcc.js
+++ src/dcc.js
@@ -15,9 +15,15 @@ class DCC {
     dcc._raw = certificateRaw;
     const base45Data = base45.decode(certificateRaw.slice(4));
     dcc._coseRaw = zlib.inflateSync(base45Data);
-    const cborPayload = cbor.decodeFirstSync(dcc._coseRaw).value[2];
+	const messageObject = cbor.decodeFirstSync(dcc._coseRaw);
+	const protectedHeader = messageObject.value[0];
+	const unprotectedHeader = messageObject.value[1];	
+    const cborPayload = messageObject.value[2];
     const jsonCBOR = cbor.decodeFirstSync(cborPayload);
     dcc._payload = jsonCBOR.get(-260).get(1);
+	var kid = cbor.decodeFirstSync(protectedHeader).get(4);
+	if(kid === undefined) kid = unprotectedHeader.get(4);
+	dcc._kid = new Buffer(kid).toString('base64');
     return dcc;
   }
 
@@ -34,6 +40,10 @@ class DCC {
 
   get payload() {
     return this._payload;
+  }
+  
+  get kid() {
+    return this._kid;
   }
 
   async checkSignature(signatureKey) {
